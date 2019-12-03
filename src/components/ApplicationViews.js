1d0028b41656ac397634c8bd862201066ff997b6
Caroline Brownlee <brownleecaroline@gmail.com>
2019-12-03 15:37:42 -0600

    worked on home page and working on rendering correct roots

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,34 +4,30 @@ import Login from "./auth/Login";
 import EventsList from "./events/EventsList";
 import EventForm from "./events/EventForm";
 import Registration from "./auth/RegisterAccount";
-
+import Home from "./home/Home";
 
 export default class ApplicationViews extends Component {
 
   render() {
     return (
       <React.Fragment>
-
+        <Route exact path="/" render={(props) => {
+          return <Home {...props} />
+        }} />
         <Route
-          exact path="/" render={props => {
-            return null
-            // Remove null and return the component which will show news articles
-          }}
-        />
-        <Route
-          exact path="/login" render={props => {
-            return <Login 
-            {...props}
-            {...this.props}
+          path="/login" render={props => {
+            return <Login setUser={this.props.setUser}
+              {...props}
+              {...this.props}
             />
           }}
         />
-        <Route
-          exact path="/register" render={props => {
-            return null
-            // Remove null and return the component which will handle user registration
-          }}
-        />
+        <Route path="/register" render={props => {
+          return <Registration setUser={this.props.setUser}
+            {...this.props}
+            {...props}
+          />
+        }} />
 
         <Route
           path="/friends" render={props => {
@@ -46,6 +42,12 @@ export default class ApplicationViews extends Component {
             // Remove null and return the component which will show the messages
           }}
         />
+        <Route
+          path="/articles" render={props => {
+            return null
+            // Remove null and return the component which will show the messages
+          }}
+        />
 
         <Route
           path="/tasks" render={props => {
@@ -53,21 +55,15 @@ export default class ApplicationViews extends Component {
             // Remove null and return the component which will show the user's tasks
           }}
         />
-      {/* EVENTS */}
+        {/* EVENTS */}
         <Route exact path="/events" render={props => {
-            return <EventsList {...props}/>
-          }}
+          return <EventsList {...props} />
+        }}
         />
         <Route path="/events/new" render={props => {
-            return <EventForm {...props}/>
-        }} 
+          return <EventForm {...props} />
+        }}
         />
-        {/* <Route path="/login" render={Login} /> */}
-        <Route exact path="/register" render={props => {
-          return <Registration {...this.props}{...props} />
-        }} />
-        
-
       </React.Fragment>
     );
   }
