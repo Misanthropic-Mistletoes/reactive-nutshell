9d995d169252724bf30ce4b5d6ce6e0ae360886b
Sam Pita <samanthakpita@gmail.com>
2019-12-04 14:15:00 -0600

    delete message feature working

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 class MessageCard extends Component {
     render() {
+        console.log("card this.props", this.props)
         return (
             <div className="message-card">
                 <div className="card-content">
                     <h5>{this.props.username}</h5>
                     <p>{this.props.message}</p>
                     <p>{this.props.timestamp}</p>
-
                 </div>
+                <button type="button" onClick={() => this.props.deleteMessage(this.props.id)}>Delete</button>
             </div>
         )
     }
