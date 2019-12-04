b1d30e079e1682fc68fee2926364730554b384f1
Caroline Brownlee <brownleecaroline@gmail.com>
2019-12-04 12:42:11 -0600

    events populate to the dom sorted by date

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -12,9 +12,9 @@ class EventCard extends Component {
     return (
       <div className="eventCard">
         <div className="card-content">
-          <h1>Title: {this.props.event.title}</h1>
-          <p>Location: {this.props.event.location}</p>
-          <p>Date: {this.props.event.date}</p>
+          <p><u><strong>{this.props.event.title}</strong></u></p>
+          <p>{this.props.event.location}</p>
+          <p>{this.props.event.date}</p>
           <button type="button" onClick={() => this.props.deleteEvent(this.props.event.id)}>Delete</button>
           <button type="button" onClick={() => { this.props.history.push(`/events/${this.props.event.id}/edit`) }}>Edit</button>
           <hr />
