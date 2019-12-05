288649237a3134c045ec84fb128f4c69df8021e9
Julian Garcia <garcia.j.julian@gmail.com>
2019-12-05 11:51:55 -0600

    added the time stamp to my article entry

--- src/components/articles/ArticlesCard.js
+++ src/components/articles/ArticlesCard.js
@@ -8,13 +8,13 @@ import './ArticlesStyles/ArticlesList.css';
 
 class ArticleCard extends Component {
   render() {
-    console.log(this.props)
     return (
       <div className="articleCard">
         <div className="card-content">
           <h1>Title: {this.props.article.title}</h1>
           <p>Synopsis: {this.props.article.synopsis}</p>
           <p>URL: {this.props.article.url}</p>
+    <p>Post date: {this.props.article.timestamp}</p>
           <button type="button" onClick={() => this.props.deleteArticle(this.props.article.id)}>
             Delete</button>
           <button type="button" onClick={() => { this.props.history.push(`/articles/${this.props.article.id}/edit`) }}>
